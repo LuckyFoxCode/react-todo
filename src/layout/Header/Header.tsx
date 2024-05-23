3a77115f5a3efe53a3b463b310720e8a48512b48
Serhii Shabala <sergei181989@gmail.com>
2024-05-23 12:42:01 +0300

    Fix: colors dark-mode

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Date, GroupButton, Logo } from './ui';
 
 export const Header = () => {
   return (
-    <header className='w-full flex items-center justify-between py-3 px-2  bg-slate-100 shadow-lg shadow-slate-200 transition-all dark:bg-slate-800 dark:shadow-slate-100 md:justify-start md:py-[20px] md:px-[35px] lg:py-[30px] lg:px-[70px]'>
+    <header className='w-full flex items-center justify-between py-3 px-2  bg-slate-100 shadow-lg shadow-slate-200 transition-all dark:bg-slate-800 dark:shadow-slate-500 md:justify-start md:py-[20px] md:px-[35px] lg:py-[30px] lg:px-[70px]'>
       <Box className='md:flex-1'>
         <Logo />
       </Box>
